3f2a98601072bd5b185c4c101528b2eee64a8b76
Fernando Salgueiro <fernando.salg@gmail.com>
2018-10-28 21:26:45 +0000

    Upgrade to JUnit 5

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,19 @@
+package guru.springframework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * Created by fernando on 28/10/18.
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class SfgPetClinicApplicationTests {
+
+  @Test
+  public void contextLoads(){
+
+  }
+}
